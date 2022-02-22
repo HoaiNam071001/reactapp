e69b9db6442370a4670f6492b26781530a9a9ce3
HoaiNam071001 <nam215543694@gmail.com>
2022-02-22 14:25:03 +0700

    sd

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,9 +5,15 @@ function App() {
   
 
   return (
-    <div className="main-home">
-      <h1>Đại học Bách Khoa - Đại học quốc gia Hồ Chí Minh</h1>
-     <iframe width="885" height="498" src="https://www.youtube.com/embed/sgoosQYaUyM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-2 col-sm-12 col-12"></div>
+          <div className="col-xl-8 col-sm-12 col-12 main-home">
+            <h1>Đại học Bách Khoa - Đại học quốc gia Hồ Chí Minh</h1>
+            <iframe width="885" height="498" src="https://www.youtube.com/embed/sgoosQYaUyM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          </div>
+        </div>
+      
     </div>
     
   );
